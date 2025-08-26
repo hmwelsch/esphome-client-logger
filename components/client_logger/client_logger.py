70e6709b4550e32f8d73733bb3a4b93ddf8b8467
Martin Welsch <admin@mwitplus.com>
2025-08-26 21:02:31 +0200

    CONFIG_SCHEMA zu client_logger.py hinzugefügt

--- components/client_logger/client_logger.py
+++ components/client_logger/client_logger.py
@@ -1,4 +1,5 @@
 import esphome.codegen as cg
 
+CONFIG_SCHEMA = cv.Schema({})
 client_logger_ns = cg.esphome_ns.namespace('client_logger')
 ClientLogger = client_logger_ns.class_('ClientLogger', cg.Component)
