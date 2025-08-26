a596c07f41df34d43c45647b55535837509c9a61
Martin Welsch <admin@mwitplus.com>
2025-08-26 21:08:06 +0200

    CONFIG_SCHEMA Reihenfolge geändert client_logger.py hinzugefügt

--- components/client_logger/client_logger.py
+++ components/client_logger/client_logger.py
@@ -1,5 +1,7 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
 
-CONFIG_SCHEMA = cv.Schema({})
 client_logger_ns = cg.esphome_ns.namespace('client_logger')
 ClientLogger = client_logger_ns.class_('ClientLogger', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({})
